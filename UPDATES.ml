4527446ea610203d6fe44631f8bf34a06e348b07
DyrexFNF <81214036+DyrexFNF@users.noreply.github.com>
2021-03-26 10:33:56 -0500

    Update UPDATES.ml

--- UPDATES.ml
+++ UPDATES.ml
@@ -1,8 +1,6 @@
----
 title: Updates
 layout: template
 filename: UPDATES
---- 
 <p>___________________________________________________________________________________________________________</p>
 <p><strong>DyrexFNF Version 1.1&frac12; (3-25-21)</strong></p>
 <p>hellooooo!! i back, I added something because why not :D</p>
